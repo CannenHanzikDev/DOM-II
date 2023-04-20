d157f106177a5e32518b293848bce34091f63be7
Cannen Hanzik <dev.cannenhanzik@gmail.com>
2023-04-20 15:56:04 -0500

    wip

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import './less/index.less'
 
-// Your code goes here!
+
+
+// 1 - load event
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO!!'
+
+    // 2 - copy
+    window.addEventListener('copy', () => {
+        navigator.clipboard.readText()
+            .then(text => {
+                heading.textContent += text;
+            })
+    })
+
+    // 3 - click event
+    document.body.addEventListener('click', evt => {
+        evt.target.classList.toggle('mirror');
+    })
+}
+
